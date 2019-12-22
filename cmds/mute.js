e7b0cf55493b7d75dc11a31d7204442288ee17b8
DriedSponge <jortuck0810@gmail.com>
2019-12-22 01:06:00 -0800

    Finished mute command

--- cmds/mute.js
+++ cmds/mute.js
@@ -1,19 +1,44 @@
-
+const Discord = require('discord.js');
 module.exports.run = async (client,msg,args) => {
-    if(msg.channel.name === 'bot-cmds'){
-        msg.reply("https://driedsponge.net/controller.php?id="+args);  
-               
-      }else{
-        msg.delete();
-        msg.reply('Please use bot commands in '+  client.channels.find(x => x.name === 'bot-cmds')).then(msg => {
-        msg.delete(7000)
-        
-        });
-    }
-    }
+    let reason = args.join(" ").slice(22);
+    if (msg.member.hasPermission(['MANAGE_MESSAGES'])) {
+        let logchannel = client.channels.find(ch => ch.name === 'discord-logs');
+        var user = msg.mentions.users.first();
+        if (user) {
+         var member = msg.guild.member(user);
+          if(member){
+            if(!member.roles.find(r => r.name === "Muted")){
+            let kickembed = new Discord.RichEmbed()
+            .setTitle(`Muted ${user.tag}`)
+            .setColor(0xFF0000)
+            .setThumbnail(user.avatarURL)
+            .addField("Admin",msg.author,true)
+            .addField("User",user,true)
+            if(reason){
+                kickembed.addField("Reason",reason,true)
+            }else{
+                kickembed.addField("Reason","Unspecified",true)
+            }
+            logchannel.send(kickembed);
+            msg.channel.send(user+" has been muted in the server!");
+            member.addRole("506963223845535756");
 
+          }else{
+            
+            msg.reply("This user is already muted!"); 
+          }
+        }else{
+          msg.reply("You did not mention a user");
+        }
+    }else{
+        msg.reply("This member does not exist");
+    }
+    }else{
+      msg.reply("Bro don't even try");
+    }
+}
 module.exports.help = {
-    name: "lookup",
-    description: "Look up any info on a steam user.",
-    syntax: "!lookup [STEAMID, CUSTOM URL]",
+    name: "mute",
+    description: "Mute a user (Admins only)",
+    syntax: "!mute [USER] [REASON]",
 }
