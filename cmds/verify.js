06b3cec9d557d19b55ad49b3ce62eaed5ee6de24
DriedSponge <jortuck0810@gmail.com>
2019-12-23 00:54:49 -0800

    Almost done with verify command, just gotta finish and fix checks

--- cmds/verify.js
+++ cmds/verify.js
@@ -1,14 +1,31 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client,msg,args,conn) => {
-  msg.reply("Check your DMs for a url to verify yourself. If you do not see a message, change your privacy settings then try again.")
-  let verifyid = Math.floor((Math.random() * 1000000) + 1);
-  conn.query(`INSERT INTO discord (discordid, verifyid) VALUES ('${msg.author.id}','${verifyid}')`, function (err, result) {
+  conn.query(`SELECT steamid FROM discord WHERE discordid = ${msg.author.id}`, function (err, check) {
     if (err) throw err;
-    console.log("1 record inserted");
+    if(check[0].steamid){
+      msg.reply(`You are already verified as https://steamcommunity.com/profiles/${check[0].steamid}. If this is not your steam account please let me or a moderator know`);
+    }else{
+    msg.reply("Check your DMs for a url to verify yourself. If you do not see a message, change your privacy settings then try again.")
+    let verifyid = Math.floor((Math.random() * 100000000) + 1);
+      if(check[0].verifyid){
+        conn.query(`UPDATE discord SET verifyid = '${verifyid}' WHERE discordid = '${msg.author.id}'`, function (err, result) {
+          if (err) throw err;
+          console.log("Record has been updated");
+          msg.author.send(`Enter ||${verifyid}|| at https://driedsponge.net/verify.php to verify your self. **DO NOT SHARE THIS CODE WITH ANYONE ELSE**`);
+        });
+      }else{
+      conn.query(`INSERT INTO discord (discordid, verifyid, discorduser) VALUES ('${msg.author.id}','${verifyid}','${msg.author.tag}')`, function (err, result) {
+        if (err) throw err;
+        console.log("Record has been inserted");
+        msg.author.send(`Enter ||${verifyid}|| at https://driedsponge.net/verify.php to verify your self. **DO NOT SHARE THIS CODE WITH ANYONE ELSE**`);
+      });
+    }
+  }
   });
-  msg.author.send(`Enter ||${verifyid}|| at https://driedsponge.net/verify.php to verify your self. **DO NOT SHARE THIS CODE WITH ANYONE ELSE**`);
+
 }
+  
 module.exports.help = {
     name: "verify",
     description: "Verify your steam account with us",
