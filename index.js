b1637eeb7a5aa77c7c4cc355261ea7b18069efda
DriedSponge <jortuck0810@gmail.com>
2020-03-06 18:44:55 -0800

    Changed welcome message

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const { Client, RichEmbed } = require('discord.js');
 
 const mysql = require('mysql');
 
-function LinkEmbed(title,url,image,desc){
-  const linkembed  = new Discord.RichEmbed()
-	.setColor('#00FF44')
-	.setTitle(title)
-	.setURL(url)
-	.setDescription(desc)
-	.setThumbnail(image)
-	.setTimestamp()
+function LinkEmbed(title, url, image, desc) {
+  const linkembed = new Discord.RichEmbed()
+    .setColor('#00FF44')
+    .setTitle(title)
+    .setURL(url)
+    .setDescription(desc)
+    .setThumbnail(image)
+    .setTimestamp()
   return linkembed;
 }
 
@@ -21,24 +21,24 @@ const fs = require("fs");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.commandshelp = new Discord.Collection();
-fs.readdir("./cmds/", (err, files)=> {
-  if(err) console.error(err);
+fs.readdir("./cmds/", (err, files) => {
+  if (err) console.error(err);
   let jsfiles = files.filter(f => f.split(".").pop() === "js");
-  if(jsfiles.length <= 0){
+  if (jsfiles.length <= 0) {
     console.log("no cmds to load!")
     return;
   }
   console.log(`Loading ${jsfiles.length} commands!`);
-  jsfiles.forEach((f,i) => {
+  jsfiles.forEach((f, i) => {
     let props = require(`./cmds/${f}`)
-    console.log(`${i +1 }: ${f} loaded!`);
+    console.log(`${i + 1}: ${f} loaded!`);
     client.commands.set(props.help.name, props);
-    client.commandshelp.set(props.help.name, [props.help.description,props.help.syntax], props);
+    client.commandshelp.set(props.help.name, [props.help.description, props.help.syntax], props);
   });
 });
 
 
-client.on('ready', () =>{
+client.on('ready', () => {
   console.log('Bot is now connected');
   client.user.setPresence({ status: 'online', game: { name: '!help | DriedSponge.net' } });
   console.log(client.commands);
@@ -46,38 +46,38 @@ client.on('ready', () =>{
 
 
 const conn = mysql.createConnection({
-  host     : 'db.hexaneweb.com',
-  port     : '3306',
-  user     : 'driedspo_netuser',
-  password : 'U7MQioT0uiaoUzzdKnR3sXEOIts4Jt08',
-  database : 'driedspo_net',
-  charset : 'utf8mb4'
+  host: 'db.hexaneweb.com',
+  port: '3306',
+  user: 'driedspo_netuser',
+  password: 'U7MQioT0uiaoUzzdKnR3sXEOIts4Jt08',
+  database: 'driedspo_net',
+  charset: 'utf8mb4'
 });
-conn.connect(function(err) {
+conn.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
 
 var minutes = 5, interval = minutes * 60 * 1000;
-setInterval(function() {
- console.log("I am doing my 5 minutes check");
+setInterval(function () {
+  console.log("I am doing my 5 minutes check");
   conn.query(`SELECT * FROM discord`, function (err, check) {
     if (err) throw err;
-    for(k in check){
-      if(check[k].givenrole == "NO" && check[k].verifyid == "VERIFIED"){
+    for (k in check) {
+      if (check[k].givenrole == "NO" && check[k].verifyid == "VERIFIED") {
         let member = client.guilds.find(g => g.id === '506684375543447573').member(check[k].discordid);
         conn.query(`UPDATE discord SET givenrole = 'YES' WHERE discordid = '${check[k].discordid}'`, function (err, result) {
           if (err) throw err;
-            member.addRole("526657280859439116");
-            let embed = new Discord.RichEmbed()
-              .setTitle(`Verification`)
-              .setColor(0x00FF44)
-              .setThumbnail(member.user.avatarURL)
-              .setDescription(`${member.user} has recieved their roles.`)
-              client.channels.find(ch => ch.id === '506832700502704148').send(embed);
+          member.addRole("526657280859439116");
+          let embed = new Discord.RichEmbed()
+            .setTitle(`Verification`)
+            .setColor(0x00FF44)
+            .setThumbnail(member.user.avatarURL)
+            .setDescription(`${member.user} has recieved their roles.`)
+          client.channels.find(ch => ch.id === '506832700502704148').send(embed);
         });
-      }else if(check[k].verifyid == "UNVERIFIED"){
+      } else if (check[k].verifyid == "UNVERIFIED") {
         let member = client.guilds.find(g => g.id === '506684375543447573').member(check[k].discordid);
         member.removeRole("526657280859439116");
         let embed = new Discord.RichEmbed()
@@ -85,10 +85,10 @@ setInterval(function() {
           .setColor(0xFF0000)
           .setThumbnail(member.user.avatarURL)
           .setDescription(`${member.user} has lost their roles!`)
-          client.channels.find(ch => ch.id === '506832700502704148').send(embed);
-      conn.query(`DELETE FROM discord WHERE discordid = '${check[k].discordid}'`, function (err, result) {
-       console.log("removed role from someone")
-      });
+        client.channels.find(ch => ch.id === '506832700502704148').send(embed);
+        conn.query(`DELETE FROM discord WHERE discordid = '${check[k].discordid}'`, function (err, result) {
+          console.log("removed role from someone")
+        });
       }
     }
   });
@@ -96,32 +96,32 @@ setInterval(function() {
 
 
 
-client.on('message', async (msg) =>{
-if(msg.author.bot) return;
-if(msg.channel.type == "dm") return;
-    let messageArray = msg.content.split(" ");
-    let command =  messageArray[0];
-    let args = messageArray.slice(1);     
-    let cmd = client.commands.get(command.slice(prefix.length));
-        if(cmd) cmd.run(client,msg,args,conn);
- 
-    if(msg.content === "!help" && msg.channel.name === 'bot-cmds'){
-      
-        const embed = new RichEmbed()
-        .setTitle('Commands')
-        .setColor(0x007BFF)
-        .setDescription('**Here is the list of all of our commands:**')
-        client.commandshelp.forEach((f,i) => {
-            embed.addField(f[1],f[0])
-          });  
-        msg.reply(embed);               
-      }else if(msg.content === "!help"){
-        msg.delete();
-        msg.reply('Please use bot commands in the bot comamnds channel').then(msg => {
-        msg.delete(7000)
-        });
-    }
-  
+client.on('message', async (msg) => {
+  if (msg.author.bot) return;
+  if (msg.channel.type == "dm") return;
+  let messageArray = msg.content.split(" ");
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
+  let cmd = client.commands.get(command.slice(prefix.length));
+  if (cmd) cmd.run(client, msg, args, conn);
+
+  if (msg.content === "!help" && msg.channel.name === 'bot-cmds') {
+
+    const embed = new RichEmbed()
+      .setTitle('Commands')
+      .setColor(0x007BFF)
+      .setDescription('**Here is the list of all of our commands:**')
+    client.commandshelp.forEach((f, i) => {
+      embed.addField(f[1], f[0])
+    });
+    msg.reply(embed);
+  } else if (msg.content === "!help") {
+    msg.delete();
+    msg.reply('Please use bot commands in the bot comamnds channel').then(msg => {
+      msg.delete(7000)
+    });
+  }
+
 
 });
 
@@ -131,31 +131,35 @@ if(msg.channel.type == "dm") return;
 
 // Remeber member represents the server
 client.on('guildMemberAdd', newmember => {
-    // Send the message to a designated channel on a server:
-    const channel = newmember.guild.channels.find(ch => ch.name === 'hello');
-    channel.send(`**Welcome to the server,** ${newmember}. We now have **${newmember.guild.memberCount}** members! To get the member role, you must type **!verify** in the bot-cmds channel and follow the instructions.`);
+  // Send the message to a designated channel on a server:
+  const channel = newmember.guild.channels.find(ch => ch.name === 'hello');
+  const welcome = new Discord.RichEmbed()
+    .setColor('#00FF44')
+    .setTitle(`${newmember.user.tag} joined the server!`)
+
+    channel.send(welcome);
     
   });
 
-  client.on('guildMemberRemove', oldmember => {
-    // Send the message to a designated channel on a server:
-    const channel = oldmember.guild.channels.find(ch => ch.name === 'discord-logs');
-    channel.send(`${oldmember} left the server. We now have **${oldmember.guild.memberCount}** members.`);
-    let logchannel = client.channels.find(ch => ch.name === 'discord-logs');
-    let kickembed = new Discord.RichEmbed()
+client.on('guildMemberRemove', oldmember => {
+  // Send the message to a designated channel on a server:
+  const channel = oldmember.guild.channels.find(ch => ch.name === 'discord-logs');
+  channel.send(`${oldmember} left the server. We now have **${oldmember.guild.memberCount}** members.`);
+  let logchannel = client.channels.find(ch => ch.name === 'discord-logs');
+  let kickembed = new Discord.RichEmbed()
     .setTitle(`Unverifed ${oldmember.user.tag}`)
     .setColor(0xFF0000)
     .setThumbnail(oldmember.user.avatarURL)
-    .addField("Admin",oldmember.user.author,true)
-    .addField("User",oldmember.user,true)
-    logchannel.send(kickembed);
-    conn.query(`DELETE FROM discord WHERE discordid=${oldmember.user.id}`, function (err, result) {
-      if (err) throw err;
-      
-    });    
+    .addField("Admin", oldmember.user.author, true)
+    .addField("User", oldmember.user, true)
+  logchannel.send(kickembed);
+  conn.query(`DELETE FROM discord WHERE discordid=${oldmember.user.id}`, function (err, result) {
+    if (err) throw err;
 
-    
   });
+
+
+});
 
 
 
